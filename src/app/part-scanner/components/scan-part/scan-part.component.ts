569dc028c13bd6140b76c14f0dfc2989769b48cd
divyanshutiwari07 <divyanshutiwari07@gmail.com>
2020-03-26 19:18:54 +0530

    Revert "Made the landscape mobile specfic"
    This reverts commit a0d1f5e064a29870f580db32dc69e52bfe278cb4.

--- src/app/part-scanner/components/scan-part/scan-part.component.ts
+++ src/app/part-scanner/components/scan-part/scan-part.component.ts
@@ -1,16 +1,12 @@
-import {Component, OnInit, OnChanges, ViewChild, Inject} from '@angular/core';
-import {DOCUMENT} from '@angular/common';
-import {WebcamImage} from 'ngx-webcam';
-import {CameraComponent} from "../camera/camera.component";
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
 
 @Component({
   selector: 'app-scan-part',
   templateUrl: './scan-part.component.html',
   styleUrls: ['./scan-part.component.scss']
 })
-export class ScanPartComponent implements OnInit {
-
-  @ViewChild('camera', {static: false}) camera: CameraComponent;
+export class ScanPartComponent implements OnInit, OnChanges {
   public webcamImages: any = [];
   public selectedOutlineImage;
   public outlineImageList;
@@ -18,51 +14,27 @@ export class ScanPartComponent implements OnInit {
   selected = 'option2';
 
   showCameraView: boolean;
-  fullScreenView: boolean;
 
   handleImage(webcamImage: WebcamImage) {
     this.webcamImage = webcamImage;
   }
 
-  constructor(@Inject(DOCUMENT) private document: any) {
-  }
+  constructor() { }
 
   ngOnInit() {
     this.showCameraView = false;
     this.outlineImageList = [
-      {image: 'Engine outline 1', src: '../../assets/images/engines-outlines/utc-engine-outline.png'},
-      {image: 'Some Other', src: 'https://via.placeholder.com/300.png/09f/fff'}
+      { image: 'Engine outline 1', src: '../../assets/images/engines-outlines/utc-engine-outline.png' },
+      { image: 'Some Other', src: 'https://via.placeholder.com/300.png/09f/fff' }
     ];
+  }
+
+  ngOnChanges() {
+
   }
 
   toggleCamera() {
     this.showCameraView = !this.showCameraView;
   }
 
-  triggerSnapshot() {
-    this.camera.triggerSnapshot();
-  }
-
-  toggleFullScreen() {
-    if ((this.document.fullScreenElement && this.document.fullScreenElement !== null) ||
-      (!this.document.mozFullScreen && !this.document.webkitIsFullScreen)) {
-      if (this.document.documentElement.requestFullScreen) {
-        this.document.documentElement.requestFullScreen();
-      } else if (this.document.documentElement.mozRequestFullScreen) {
-        this.document.documentElement.mozRequestFullScreen();
-      } else if (this.document.documentElement.webkitRequestFullScreen) {
-        this.document.documentElement.webkitRequestFullScreen(Element['ALLOW_KEYBOARD_INPUT']);
-      }
-      this.fullScreenView = true;
-    } else {
-      if (this.document.cancelFullScreen) {
-        this.document.cancelFullScreen();
-      } else if (this.document.mozCancelFullScreen) {
-        this.document.mozCancelFullScreen();
-      } else if (this.document.webkitCancelFullScreen) {
-        this.document.webkitCancelFullScreen();
-      }
-      this.fullScreenView = false;
-    }
-  }
 }
