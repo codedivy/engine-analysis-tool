66c3c9949f0ddd731ae25a2094afdf564a2de4c2
divyanshutiwari07 <divyanshutiwari07@gmail.com>
2020-04-01 19:41:05 +0530

    Added post lease table in scan part

--- src/app/part-scanner/components/lease/common/post-lease-table/post-lease-table.component.ts
+++ src/app/part-scanner/components/lease/common/post-lease-table/post-lease-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
 
 @Component({
   selector: 'app-post-lease-table',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostLeaseTableComponent implements OnInit {
 
-  constructor() { }
+  @Input() data: any = [];
+  @Input() tableHeading;
+
+  @ViewChild(MatSort, {static: false}) set matSort(sort: MatSort) {
+    this.dataSource.sortingDataAccessor = (data: any, sortHeaderId: string): string => {
+      if (typeof data[sortHeaderId] === 'string') {
+        return data[sortHeaderId].toLocaleLowerCase();
+      }
+      return data[sortHeaderId];
+    };
+    this.dataSource.sort = sort;
+  }
+
+  @ViewChild(MatPaginator, {static: false}) set paginator(sort: MatPaginator) {
+    this.dataSource.paginator = sort;
+  }
+
+  public cssClassForStatus = {
+    P: 'pending',
+    M: 'missing',
+    D: 'done'
+  };
+
+  public displayedColumns: string[] = ['position', 'partName', 'gridLocation', 'totalCount', 'serialNumber', 'photo', 'preLeaseStatus', 'postLeaseStatus'];
+  public dataSource: any;
+
+  constructor() {
+    this.dataSource = new MatTableDataSource([]);
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.dataSource.data = this.data;
   }
 
 }
